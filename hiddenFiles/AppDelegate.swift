a616239af82da4db404593044a223e3afd0a0779
Markus Mächler <markusmaechler@gmx.net>
2015-01-19 22:29:17 +0100

    Initial Commit

---
+++ hiddenFiles/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  hiddenFiles
+//
+//  Created by Markus Mächler on 19/01/15.
+//  Copyright (c) 2015 Markus Mächler. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
